98d5dffa65a3b9f8ce72ad7fabc1f499056f90ec
Maruf Hasan <marufhasanmunshi@gmail.com>
2017-10-03 22:26:12 +0600

    background image slide show done.

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,3 +9,24 @@ $('#owl-two').owlCarousel({
     margin:10,
     items:1,
 });
+
+// var images=new Array('resources/css/img/DSC_0216.JPG','resources/css/img/DSC_0824.JPG');
+// var nextimage=0;
+// doSlideshow();
+
+// function doSlideshow(){
+//     if(nextimage>=images.length){nextimage=0;}
+//     $('.global-header')
+//     .css('background-image',
+//     'linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, 0.5)), url("'+images[nextimage++]+'")')
+//     .fadeIn('.10000',function(){
+//         setTimeout(doSlideshow,7000);
+//     });
+// }
+
+$(".global-header").vegas({
+    slides: [
+        { src: "resources/css/img/DSC_0216.JPG" },
+        { src: "resources/css/img/DSC_0824.JPG" },
+    ],
+});
